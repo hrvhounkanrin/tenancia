eba4a807b779c1dcfd671570a38a75de4d2fe7d5
Hervé HOUNKANRIN <hrvhounkanrin@gmail.com>
2018-12-17 14:12:38 +0100

    Add differents packages for models and remove mcore/model

---
+++ meslimmo/immeuble/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from countries_plus.models import Country
+
+
+class Immeuble():
+    intitule = models.CharField(max_length=50)
+    description = models.CharField(max_length=512, null=True, )
+    adresse = models.CharField(max_length=512, null=True, )
+    proprietaire = models.ForeignKey("prorietaire.Proprietaire", on_delete=models.SET_NULL, null=False)
+    jour_emission_facture = models.IntegerField(default=5)
+    jour_valeur_facture = models.IntegerField(default=5)
+    ville = models.CharField(max_length=128, null=True)
+    quartier = models.CharField(max_length=128, null=True)
+    pays = models.ForeignKey(Country, null=True, on_delete=models.SET_NULL)
+    longitude = models.DecimalField(max_digits=20, decimal_places=12)
+    latitude = models.DecimalField(max_digits=20, decimal_places=12)
+    """Numéro d'identification donné par la mairie ou les autorités locales"""
+    ref_immeuble = models.CharField(max_length=50, null=True)
+
+    def __str__(self):
+        return '%s %s' % (self.intitule, self.proprietaire.user.last_name)
---
+++ meslimmo/quittance/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuittanceConfig(AppConfig):
+    name = 'quittance'
