9e3285b01fb373ae45dbed84adbd55e12c4fb54e
Hervé HOUNKANRIN <hrvhounkanrin@gmail.com>
2018-12-18 13:49:29 +0100

    Remove mcore/serializer folder

--- meslimmo/mcore/serializers/composantAppartementSerializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from mcore.models import ComposantAppartement
-
-class  ComposantAppartementSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model=ComposantAppartement
-        fields = '__all__'
