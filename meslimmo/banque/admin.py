6b49b637d95b7f04923ae946996401338a7c16bf
Boris <patchocharley@gmail.com>
2018-12-18 13:47:22 +0200

    Still organizing ...

---
+++ meslimmo/banque/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here
+
+# from meslimmo.banque.models import  Banque
+#
+# class BanqueAdmin(admin.ModelAdmin):
+#     fields =('codebanque', 'libbanque', 'pays')
+#
+#
+# admin.site.register(Banque, BanqueAdmin)
---
+++ meslimmo/proprietaire/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+# from meslimmo.meslimmo.proprietaire.models import  *
+# Register your models here.
+# class ProprietaireAdmin(admin.ModelAdmin):
+#     fields = ('mode_paiement', 'numcompte')
+
