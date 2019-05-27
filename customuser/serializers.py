50c24f9af374c0c0603ac29e2b80f926d4218d1e
Boris <boris@dotmodus.com>
2019-05-27 12:43:00 +0200

    stripped out dob , city and zip

--- customuser/serializers.py
+++ customuser/serializers.py
@@ -5,7 +5,7 @@ from customuser.models import User, UserProfile
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
-        fields = ('title', 'dob', 'address', 'country', 'city', 'zip', 'photo')
+        fields = ('title', 'address', 'country', 'photo')
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
