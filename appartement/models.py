3d230a1f4e58e84d2dd9028870c7f8442fc1bda6
Hervé HOUNKANRIN <hrvhounkanrin@gmail.com>
2019-09-10 13:13:59 +0100

    new fields to models

--- appartement/models.py
+++ appartement/models.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 """Housing app models."""
+from django.conf import settings
 from django.db import models
 
 
@@ -8,6 +9,14 @@ class ComposantAppartement(models.Model):
 
     libelle = models.CharField(max_length=50)
     utilite = models.CharField(max_length=256, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        editable=False, related_name='created_user')
+    modified_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        editable=False, related_name='updated_user')
 
 
 class Appartement(models.Model):
@@ -23,7 +32,7 @@ class Appartement(models.Model):
         (RESERVE, 'RESERVE'),
         (OCCUPE, 'OCCUPE'),
         (BIENTOT_LIBRE, 'BIENTOT_LIBRE')
-        )
+    )
     intitule = models.CharField(max_length=50)
     """level le niveau de l'appartement sur l'immeuble."""
     level = models.IntegerField(null=False)
@@ -36,10 +45,18 @@ class Appartement(models.Model):
         related_name='structure', blank=False)
     statut = models.CharField(max_length=50,
                               choices=STATUT_APPARTEMENT, default='LIBRE')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        editable=False, related_name='created_user')
+    modified_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        editable=False, related_name='updated_user')
 
     def __str__(self):
         """Housing reprensentation."""
-        return 'Immeuble: intitule {0}'.format(self.intitule, )
+        return 'Immeuble: intitule {0}'.format(self.intitule,)
 
 
 class StructureAppartement(models.Model):
@@ -54,3 +71,11 @@ class StructureAppartement(models.Model):
     nbre = models.IntegerField(default=1)
     description = models.CharField(max_length=256)
     is_periodic = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        editable=False, related_name='created_user')
+    modified_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, null=True,
+        editable=False, related_name='updated_user')
